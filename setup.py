f6e2769eaab05ed29e7a1eba9c6ad1b6f269f06e
Inosuke Hashibira <99578162+bilbilaki@users.noreply.github.com>
2025-07-27 15:15:33 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author="Leandro César",
     author_email="ccleandroc@gmail.com",
     url="https://github.com/leandcesar/themoviedb",
-    version="{{VERSION_PLACEHOLDER}}",
+    version="1",
     license="MIT",
     python_requires=">=3.8",
     packages=find_packages(include=["themoviedb", "themoviedb.*"]),
